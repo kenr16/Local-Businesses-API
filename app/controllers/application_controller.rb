47205c63a0b1e6f4ee2a41205e0611c1ee173232
Ken Rutan <kenr16@gmail.com>
2017-07-21 13:31:11 -0700

    added exception handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::API
+  include Response
+  include ExceptionHandler
 
   rescue_from ActiveRecord::RecordNotFound do |exception|
     json_response({ message: exception.message }, :not_found)
   end
-  
+
 end
